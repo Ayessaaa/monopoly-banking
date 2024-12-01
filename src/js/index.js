318848a5ee60d1c176586a55363c97a59750b1f4
Ayessa <ayessamaedeguzman78@gmail.com>
2024-12-01 20:45:31 +0800

    backend finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 window.onload = function () {
-  console.log("as");
+  
+  var rfids = []
+  var colors = ["red", "blue", "violet", "green", "yellow", "lime", "pink", "sky", "orange", "teal"]
   const rfidInput = document.getElementById("rfid");
   let scanIndex = 0;
 
@@ -21,7 +23,24 @@ window.onload = function () {
       console.log(rfidInput.value);
       const scan = document.getElementById("scan"+scanIndex);
       scan.innerText = "registered!";
+      rfids.push([rfidInput.value, scanIndex])
       rfidInput.value = "";
+      
     }
   }
+
+  const nextPage = document.getElementById("nextPage")
+  nextPage.addEventListener("click", function(){
+    const players = {
+    };
+    rfids.forEach(element => {
+      players[element[0]] = [document.getElementById("name"+element[1]).value, colors[element[1]-1], 1500]
+    });
+    var tr = sessionStorage.setItem("tbody", "");
+    sessionStorage.setItem("players", JSON.stringify(players));
+    window.location.href = "playertransfer.ejs";
+  })
+
+
+  
 };
